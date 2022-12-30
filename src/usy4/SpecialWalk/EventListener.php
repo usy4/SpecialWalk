f0626e75a3bb81cd8435701a7aafe660d322069c
usy4 <68358524+usy4@users.noreply.github.com>
2022-12-30 21:24:14 +0300

    Update EventListener.php

--- src/usy4/SpecialWalk/EventListener.php
+++ src/usy4/SpecialWalk/EventListener.php
@@ -75,10 +75,8 @@ class EventListener implements Listener
         $p = $block->getPosition();
         $alll = ($p->getX() . $p->getY() . $p->getZ());
         foreach(Main::$blocks as $keys => $values){
-            var_dump($alll !== $values[4]);
-            var_dump($values[4]);
-            var_dump($alll);
-            if ($alll !== $values[4]) return;
+            var_dump(in_array($alll, $keys));
+            if (!in_array($alll, $keys)) return;
             $event->cancel();
         }
     }
